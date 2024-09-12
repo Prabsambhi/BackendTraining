129aab7588d2e4e011686e24f005f6dd8a878ccf
Prabsambhi <prabsambhi@gmail.com>
2024-09-12 16:39:07 +0530

    new

--- Frontend/src/Pages/Auth/Register.jsx
+++ Frontend/src/Pages/Auth/Register.jsx
@@ -114,7 +114,7 @@ const Register = () => {
 
         <label>Email</label>
         <input
-          type="email"
+          type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
